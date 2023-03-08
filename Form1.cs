7861ca0bbd187708cf32c41319e1daa4f95c94bf
NinoMomic <58861649+NinoMomic@users.noreply.github.com>
2023-03-08 09:14:21 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace WindowsFormsCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Osoba> listaOsoba = new List<Osoba>();
+
+            String file = @"C:\Users\ucenik\Desktop\NinoMomic 3.D\WinForms.csv";
+            String separator = ",";
+            StringBuilder output = new StringBuilder();
+            String[] headings = { "Ime", "Prezime", "E-mail", "GodinaRođenja" };
+            output.AppendLine(string.Join(separator, headings));
+
+            try
+            {
+                Osoba osoba = new Osoba(txtIme.Text, txtPrezime.Text, 
+                    txtemail.Text, Convert.ToInt16(txtgodRod.Text));
+                txtIme.Clear();
+                txtPrezime.Clear();
+                txtemail.Clear();
+                txtgodRod.Clear();
+
+                DialogResult upit = MessageBox.Show("Želite li unesti još podataka" + 
+                    MessageBoxButtons.YesNo + MessageBoxIcon.Question);
+
+                switch(upit)
+                {
+                    case DialogResult.Yes:
+                    {
+                        break;
+                    }
+                    case DialogResult.No:
+                    {
+                        listaOsoba.Add(osoba);
+
+                        foreach(Osoba os in listaOsoba)
+                        {
+                                String[] newLine = { txtIme.Text, txtPrezime.Text,
+                                         txtemail.Text, txtgodRod.ToString()};
+                                output.AppendLine(string.Join(separator, newLine));
+
+                                try
+                                {
+                                    File.AppendAllText(file, output.ToString());
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("Data could not be written to the CSV file.");
+                                    return;
+                                }
+                            }
+
+                        break;
+                    }
+
+                }
+
+            } catch (Exception greska)
+            {
+                MessageBox.Show(greska.Message, "Pogrešan unos",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ Osoba.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsCSV
+{
+    internal class Osoba
+    {
+        string ime, prezime, email;
+        int godRod;
+
+        public override string ToString()
+        {
+            string ispis = "Ime: " + this.Ime +
+                "Prezime: " + this.Prezime +
+                "Godina rođenja: " + this.GodRod +
+                "E-mail: " + this.Email;
+
+
+                return ispis;
+        }
+
+        public Osoba(string ime, string prezime, string email, int godRod)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.email = email;
+            this.godRod = godRod;
+        }
+        public string Ime  { get => ime; set => ime = value; }
+        public string Prezime { get => prezime; set => prezime = value; }
+        public string Email { get => email; set => email = value; }
+        public int GodRod { get => godRod; set => godRod = value; }
+    }
+}
